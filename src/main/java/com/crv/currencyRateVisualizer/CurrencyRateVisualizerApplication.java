b328c15b02857141491fdf0c21cd258f83a654dd
Szymon Kruszynski <kruszon4@gmail.com>
2019-05-17 20:24:35 +0200

    Initial commit

---
+++ src/main/java/com/crv/currencyRateVisualizer/CurrencyRateVisualizerApplication.java
@@ -0,0 +1,13 @@
+package com.crv.currencyRateVisualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyRateVisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyRateVisualizerApplication.class, args);
+	}
+
+}
